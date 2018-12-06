d019fec134885dd7d0c24746bb8f3628f7d26736
ubermidas <45646388+ubermidas@users.noreply.github.com>
2018-12-05 21:43:50 -0500

    Create ki.lua

---
+++ ki.lua
@@ -0,0 +1,78 @@
+{
+  label = "KI",
+  shapes = {
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+    { 0,4,4,10,16,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 10,4,4,16,5,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 1,5,3,2,16,4,texture = "iron_block", tint = 0x009900 },
+    { 7,5,3,8,6,4,texture = "iron_block", tint = 0x009900 },
+    { 10,5,3,16,6,5,texture = "iron_block", tint = 0x009900 },
+    { 6,6,3,7,7,4,texture = "iron_block", tint = 0x009900 },
+    { 10,6,4,14,15,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 14,6,3,15,16,5,texture = "iron_block", tint = 0x009900 },
+    { 15,6,4,16,15,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 5,7,3,6,8,4,texture = "iron_block", tint = 0x009900 },
+    { 9,7,3,10,8,4,texture = "iron_block", tint = 0x009900 },
+    { 4,8,3,5,9,4,texture = "iron_block", tint = 0x009900 },
+    { 10,8,3,11,9,4,texture = "iron_block", tint = 0x009900 },
+    { 3,9,3,4,12,4,texture = "iron_block", tint = 0x009900 },
+    { 11,9,3,12,12,4,texture = "iron_block", tint = 0x009900 },
+    { 2,10,3,13,11,4,texture = "iron_block", tint = 0x009900 },
+    { 4,12,3,5,13,4,texture = "iron_block", tint = 0x009900 },
+    { 10,12,3,11,13,4,texture = "iron_block", tint = 0x009900 },
+    { 5,13,3,6,14,4,texture = "iron_block", tint = 0x009900 },
+    { 9,13,3,10,14,4,texture = "iron_block", tint = 0x009900 },
+    { 6,14,3,7,15,4,texture = "iron_block", tint = 0x009900 },
+    { 7,15,3,8,16,4,texture = "iron_block", tint = 0x009900 },
+    { 10,15,3,16,16,5,texture = "iron_block", tint = 0x009900 },
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+    { 0,0,4,16,1,5,texture = "iron_block", tint = 0xbbbbbb },
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+    { 0,4,4,4,5,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 0,5,3,3,6,5,texture = "iron_block", tint = 0x009900 },
+    { 3,5,4,4,16,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 0,6,4,4,15,5,texture = "iron_block", tint = 0xbbbbbb },
+    { 1,7,3,2,14,4,texture = "iron_block", tint = 0x009900 },
+    { 0,15,3,3,16,5,texture = "iron_block", tint = 0x009900 },
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+  }
+},
+
+{
+  label = "KI",
+  shapes = {
+    { 0,0,4,4,1,5,texture = "iron_block", tint = 0xbbbbbb },
+  }
+}
